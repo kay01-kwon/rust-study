40e37d2a1505868671333a420d75af165b341a10
kay <kay1216@yonsei.ac.kr>
2024-05-30 18:46:52 +0900

    updated

--- chap4/slice_type/src/main.rs
+++ chap4/slice_type/src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
+    // To get index through reference 
+    // is problematic
     let mut s = String::from("hello world");
     let word = first_word(&s);
+    let (first_index, last_index) = second_word(&s);
     s.clear();
-    println!("{}",word);
+
+    println!("Space starts at {}",word);
     // But, the string is already deleted
     // You cannot access to the String variable s
+    println!("{}, {}",first_index, last_index);
+
+
+    // String slice
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
 }
 
 fn first_word(s:&String) -> usize
@@ -20,4 +32,24 @@ fn first_word(s:&String) -> usize
     }
     // Otherwise, return the length of the string
     s.len()
+}
+
+fn second_word(s: &String) -> (usize, usize) 
+{
+    let bytes = s.as_bytes();
+    let first_index;
+    let last_index;
+
+    first_index = 0;
+    for(i,&item) in bytes.iter().enumerate()
+    {
+        // Return the index of back space
+        if item == b' '{
+            let first_index = i;
+        }
+    }
+
+    last_index = s.len();
+
+    (first_index, last_index)
 }
